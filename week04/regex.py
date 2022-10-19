43c20e5dc3b50dbf7d17cd6c7285008ebd5fc00e
RACHEL KAUFMAN <71035742+rkaufman8@users.noreply.github.com>
2022-10-19 23:34:23 +0000

    week07 - websites.md

--- week04/regex.py
+++ week04/regex.py
@@ -1,18 +1,28 @@
 import re
+#import reg ex library
 
+def find_name(line): #creating namefinder function
+  
+    # Names with 2 or 3 words (meaning a capital letter followed by lower case letters)
+    pattern = r"([A-Za-z]+ [A-Za-z]+(?: [A-Za-z]+)?)"
+    result = re.findall(pattern, line)
 
-def find_name(line):
-    #pattern = r"\d{1,2}/\d{1,2}/\d{2,4}"
-    #result = re.findall(pattern,line)
+    # Names with a title
+    pattern = r'(?:Mrs\.|Mr\.|Dr\.|Ms\.) [A-Z][a-z]+'
+    result = result + re.findall(pattern, line)
 
-    pattern=[A-Z]([a-z]+|\.)(?:\s+[A-Z]([a-z]+|\.))*(?:\s+[a-z][a-z\-]+){0,2}\s+[A-Z]([a-z]+|\.)
-    result = result + re.findall(pattern,line)
+    #Names with single letters 
+    pattern = r'(Dr\. [A-Z]\. [A-Z][a-z]+|[A-Z]\. ?[A-Z]\. [A-Z][a-z]+|[A-Z][a-z]+ [A-Z]\. [A-Z][a-z]+|[A-Z]\. [A-Z][a-z]+)'
+    result = result + re.findall(pattern, line)
+
     return result
 
 
 f = open("names.txt")
 for line in f.readlines():
-    #print(line)
+    # print(line)
     result = find_name(line)
-    if (len(result)>0):
+    if (len(result) > 0):
         print(result)
+
+
---
+++ week07/websites.md
@@ -0,0 +1,5 @@
+repo holding website code:
+https://github.com/rkaufman8/rkaufmanwebsite/blob/main/index.html
+
+link to live website:
+https://rkaufman8.github.io/rkaufmanwebsite/
